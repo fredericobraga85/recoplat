ff993467a927eb61876b5577aa7393cad8c3cba5
Fred Braga <fred@MacBook-Pro-de-Frederico.local>
2019-01-22 00:39:54 -0200

    Lesson 8 Route Files w/ Express Router

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 const db = require("./config/keys").mongodbURI;
@@ -15,6 +19,10 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
