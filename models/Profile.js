b7f25d4ecbb24a033c66110bca2003e5185d0a0d
Fred <fred@MacBook-Pro-de-Frederico.local>
2019-01-25 18:18:37 -0200

    Lesson 18 Create & Update Profile

--- models/Profile.js
+++ models/Profile.js
@@ -24,15 +24,15 @@ const ProfileSchema = new Schema({
     type: String,
     required: true
   },
-  skills: {
-    type: [String],
-    required: true
-  },
   bio: {
     type: String
   },
   githubusername: {
     type: String
+  },
+  skills: {
+    type: [String],
+    required: true
   },
   experience: [
     {
@@ -63,7 +63,7 @@ const ProfileSchema = new Schema({
       }
     }
   ],
-  expereducation: [
+  education: [
     {
       school: {
         type: String,
@@ -91,7 +91,28 @@ const ProfileSchema = new Schema({
         type: String
       }
     }
-  ]
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = Profile = mongoose.model("profile", ProfileSchema);
