2fb6bdbc6710439fc9ccac74035c4c4b952fe649
Fred <fred@MacBook-Pro-de-Frederico.local>
2019-01-28 14:35:46 -0200

    Lesson 21 More Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,6 +38,67 @@ router.get(
   }
 );
 
+//@route GET /api/profile/handle/:handle
+//@desc get profile by handle
+//@access Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.profile = "There is no profile with that handle.";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile with that handle." })
+    );
+});
+
+//@route GET /api/profile/user/:user_id
+//@desc get profile by user id
+//@access Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.profile = "There is no profile with that id.";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile with that id." })
+    );
+});
+
+//@route GET /api/profile/all
+//@desc get all profiles
+//@access Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.profile = "There are no profiles.";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles." }));
+});
+
 //@route Post /api/profile
 //@desc create or update profile
 //@access Private
