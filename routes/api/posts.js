7cd90d1dc3456ef8100df0ef3c1a33453682fafa
Fred <fred@MacBook-Pro-de-Frederico.local>
2019-01-29 16:03:17 -0200

    Lesson 25 Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,12 +6,47 @@ const Post = require("../../models/Post");
 //validation
 const validatePostInput = require("../../validation/post");
 
-//@route GET /api/posts/test
-//@desc tests posts route
+//@route GET /api/posts/
+//@desc Get all posts sorted by most recent
 //@access Public
-router.get("/test", (req, res) => {
-  res.json({ msg: "Posts test Ok!" });
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => res.status(404).json({ post: "No posts found" }));
 });
+
+//@route GET /api/posts/
+//@desc Get all posts sorted by most recent
+//@access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({ post: "No post found" });
+      }
+      res.json(post);
+    })
+    .catch(err => res.status(400).json({ post: "No post found" }));
+});
+
+//@route DELETE /api/posts/
+//@desc Delete post
+//@access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findOne({ _id: req.params.id, user: req.user.id })
+      .then(post => {
+        console.log(post);
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(400).json({ post: "Could not delete post" }));
+  }
+);
 
 //@route Post /api/posts/\
 //@desc Create a Post
