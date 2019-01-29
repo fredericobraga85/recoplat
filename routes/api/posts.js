534378475366b612ec538a38ba6c0cc70fe39f07
Fred <fred@MacBook-Pro-de-Frederico.local>
2019-01-29 15:22:50 -0200

    Lesson 25 Creat Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const Post = require("../../models/Post");
+
+//validation
+const validatePostInput = require("../../validation/post");
 
 //@route GET /api/posts/test
 //@desc tests posts route
@@ -7,5 +12,38 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Posts test Ok!" });
 });
+
+//@route Post /api/posts/\
+//@desc Create a Post
+//@access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //validate post
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost
+      .save()
+      .then(post => {
+        res.json(post);
+      })
+      .catch(err => {
+        errors.post = "Could not create post";
+        res.status(400).json(errors);
+      });
+  }
+);
 
 module.exports = router;
