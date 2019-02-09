469f66dd2c127a5ed49ce5b285d5eb9b575ac83e
Frederico Braga <frederico.braga@x-team.com>
2019-02-09 17:02:18 -0200

    Lesson 48 Spinner Component & Dashboard Start

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,26 +2,64 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getProfile } from '../../actions/profileActions'
 import PropTypes from 'prop-types'
+import Spinner from '../common/Spinner'
+import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getProfile()
   }
-
   render() {
+    const { user } = this.props.auth
+    const { profile, loading } = this.props.profile
+
+    let dashboardContent
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4> TBD Displays dashboard</h4>
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="load text-muted">Welcome {user.name} !</p>
+            <p>You have not yet setup a profile, please add some info.</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        )
+      }
+    }
+
     return (
       <div>
         <h2>Dashboard!</h2>
+        <div className="dashboard">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">{dashboardContent}</div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 Dashboard.propTypes = {
-  getProfile: PropTypes.func.isRequired
+  getProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 }
 
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+})
+
 export default connect(
-  null,
+  mapStateToProps,
   { getProfile }
 )(Dashboard)
